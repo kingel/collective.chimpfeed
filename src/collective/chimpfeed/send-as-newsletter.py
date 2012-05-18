2e0545cdba324086a16457ef150c4b0023937505
Franklin Kingma <franklin@fourdigits.nl>
2012-05-18 16:47:45 +0200

    Lots of things Use lxml to get the content (again) Get email and sender from plone properties

--- src/collective/chimpfeed/send-as-newsletter.py
+++ src/collective/chimpfeed/send-as-newsletter.py
@@ -11,6 +11,8 @@ from collective.chimpfeed.interfaces import IFeedSettings
 from collective.chimpfeed.form import BaseForm
 from collective.chimpfeed import MessageFactory as _
 
+from Products.CMFCore.utils import getToolByName
+
 # from plone.formwidget.datetime.z3cform.widget import DatetimeFieldWidget
 from collective.z3cform.datetimewidget import DatetimeFieldWidget
 
@@ -59,20 +61,37 @@ class SendAsNewsletter(BaseForm):
         api_key = settings.mailchimp_api_key
         api = greatape.MailChimp(api_key, debug=True)
 
+        portal_url = getToolByName(self, 'portal_url')
+        portal = portal_url.getPortalObject()
+        import pdb; pdb.set_trace( )
+        from_email = portal.getProperty('email_from_address')
+        from_name = portal.getProperty('email_from_name')
+
         options = {"list_id": data['list'],
                    "subject": self.context.title,
-                   "from_email": "franklin@fourdigits.nl",
-                   "from_name": "Franklin Kingma",
-                   "auto_footer": "True",
+                   "from_email": from_email,
+                   "from_name": from_name,
+                   "auto_footer": "False",
                    "template_id": data['template'],
                      }
 
-        try:
-            content = self.context.getText()
-        except:
-            content = self.context[self.context.default_page].getText()
+        content = self.context()
+        content = content.encode('utf-8')
+        document = lxml.html.fromstring(content)
+        content = document.cssselect("div#content-core")
 
-        content = {'html_MAIN': content,
+        # inspiration: ?? plone_html_strip
+        # https://svn.plone.org/svn/collective/collective.dancing/trunk/collective/dancing/composer.py
+        to_remove = ['.documentModified',
+                     '.documentByLine']
+
+        for remove_element in to_remove:
+            remove_me = content[0].cssselect(remove_element)
+            if remove_me:
+                for element in remove_me:
+                    element.getparent().remove(element)
+
+        content = {'html_MAIN': lxml.html.tostring(content[0]),
                    'generate_text': True}
 
         campaign_id = api.campaignCreate(type="regular",
